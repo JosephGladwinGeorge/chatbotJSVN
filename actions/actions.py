0135321933b74c12925180bb3ea42c8330f99f3c
JosephGladwinGeorge <jgladwin27.2002@gmail.com>
2023-06-04 21:27:43 +0530

    some changes

--- actions/actions.py
+++ actions/actions.py
@@ -33,10 +33,50 @@ import mysql.connector
 from mysql.connector import Error
 print("started")
 
-class ActionSessionStart(Action):
-    def name(self) -> Text:
-        return "action_session_start"
+# class ActionSessionStart(Action):
+#     def name(self) -> Text:
+#         return "action_session_start"
 
+#     def run(self, dispatcher: CollectingDispatcher,
+#             tracker: Tracker,
+#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+#         student_id = tracker.sender_id
+#         print(student_id)
+
+#         try:
+#             # Connect to the MySQL database
+#             connection = mysql.connector.connect(
+#                 host='localhost',
+#                 user='root',
+#                 password='avengers007',
+#                 database='studentdata'
+#             )
+
+#             # Execute your SQL query using the conversation_id
+#             query = f"SELECT mark FROM result WHERE student_id = '{student_id}'"
+#             cursor = connection.cursor()
+#             cursor.execute(query)
+
+#             # Fetch the results
+#             results = cursor.fetchall()
+
+#             # Perform any necessary actions with the query results
+#             print(results)
+#             dispatcher.utter_message("mark = ",results)
+#             # Close the cursor and database connection
+#             cursor.close()
+#             connection.close()
+
+#         except Error as e:
+#             # Handle any errors that occur during the database connection or query execution
+#             print(f"Error connecting to MySQL: {e}")
+
+#         return []
+
+class ActionCgpa(Action):
+    def name(self) -> Text:
+        return "action_cgpa"
+    
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
@@ -44,51 +84,13 @@ class ActionSessionStart(Action):
         print(student_id)
 
         try:
-            # Connect to the MySQL database
             connection = mysql.connector.connect(
                 host='localhost',
                 user='root',
                 password='avengers007',
                 database='studentdata'
             )
-
-            # Execute your SQL query using the conversation_id
-            query = f"SELECT mark FROM result WHERE student_id = '{student_id}'"
-            cursor = connection.cursor()
-            cursor.execute(query)
-
-            # Fetch the results
-            results = cursor.fetchall()
-
-            # Perform any necessary actions with the query results
-            print(results)
-            dispatcher.utter_message("mark = ",results)
-            # Close the cursor and database connection
-            cursor.close()
-            connection.close()
-
-        except Error as e:
-            # Handle any errors that occur during the database connection or query execution
-            print(f"Error connecting to MySQL: {e}")
-
-        return []
-
-class ActionCgpa(Action):
-    def name(self) -> Text:
-        return "action_cgpa"
-    
-    def run(self, dispatcher:CollectingDispatcher, tracker: Tracker):
-        student_id = tracker.sender_id
-        print(student_id)
-
-        try:
-            connection = mysql.connector.connect(
-                host='localhost',
-                user='root',
-                password='avengers007',
-                database='studentdata'
-            )
-            query = f"SELECT year_1_cgpa FROM result WHERE user_id = '{student_id}'"
+            query = f"SELECT year_1_cgpa FROM main_table WHERE user_id = '{student_id}'"
             cursor = connection.cursor()
             cursor.execute(query)
 
@@ -96,7 +98,7 @@ class ActionCgpa(Action):
 
             # Perform any necessary actions with the query results
             print(results)
-            print(int(results))
+            #print(int(results))
             dispatcher.utter_message("your cgpa is ",results)
             cursor.close()
             connection.close()
